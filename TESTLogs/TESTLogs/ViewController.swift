ddecffc82f796a1d5df0fb918f9df64fad890b3d
Pavan Baddeputi <pavankumar.baddeputi@resideo.com>
2023-01-14 16:42:34 +0530

    Sample Project Added

---
+++ TESTLogs/TESTLogs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TESTLogs
+//
+//  Created by Pavan Baddeputi on 14/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
